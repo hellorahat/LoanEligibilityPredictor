dcf8dd2c28fda25a40959f7cb4a29545941b4576
Victor Li <113569505+qasdasd1020@users.noreply.github.com>
2024-05-09 23:47:01 -0400

    Update Node.cpp

--- src/CoreLogic/Node.cpp
+++ src/CoreLogic/Node.cpp
@@ -1,6 +1,19 @@
-#include "Node.h"
+#include <iostream>
+#include <vector>
+#include <memory>
 
-class Node {
-    public:
+using namespace std;
+
+//create the class for the nodes.
+class Node{
+public:
+    Node():left(nullptr), right(nullptr), feature_index(-1), threshold(0.0), is_leaf(false), label(-1), gini_index(0){}    // Constructor for the nodes.
+
+    unique_ptr<Node> left;
+    unique_ptr<Node> right;
+    int feature_index;
+    double threshold;
+    bool is_leaf;
+    int label;
     int gini_index;
-};
+}
