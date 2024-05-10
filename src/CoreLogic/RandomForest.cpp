7eaf81dca4ba13db2220b0c83e20166562ea533e
Victor Li <113569505+qasdasd1020@users.noreply.github.com>
2024-05-10 01:08:50 -0400

    Update RandomForest.cpp

--- src/CoreLogic/RandomForest.cpp
+++ src/CoreLogic/RandomForest.cpp
@@ -1,34 +1,37 @@
 //RandomForest.cpp
 
 #include <iostream>
+#include <vector>
 #include <cstdlib>
-#include "RandomForest.h"
 
 using namespace std;
 
-RandomForest::RandomForest(int num_trees) : num_trees_(num_trees), trees_(num_trees){}
-
-void RandomForest::train(const vector <vector<double>>& features, const vector<int>& labels) {
-  cout << "Training " << num_trees_ << " trees with bagging... " <<endl;
-  int num_samples = features.size();
-  for (auto& tree : trees_) {
-    vector<vector<double>> sample_features;
-    vector<int> sample_labels;
-    for (size_t i = 0; i < num_samples; ++i){
-      size_t idx=rand() % num_samples;
-      sample_features.push_back (features[idx]);
-      sample_labels.push_back (labels[idx]);
-    }
-  tree.train(sample_features, sample_labels);
+class randomForest{
+public:
+  randomForest (int num_trees) : num_trees_(num_trees), tree_(num_trees) {}
+  void train (const vector<vector<double>> data_vec){
+      cout <<"Training " << num_trees_ << "trees  with bagging... " << endl;
+      int num_samples = data_vec.size();
+      for (auto& tree:trees_){
+          vector<vector<double>> sample_data_vec;
+          for (size_t i = 0; i < num_samples; i++){
+              size_t idx = rand() % num_samples;
+              sample_data_vec.push_back(data_vec[idx]);
+          }
+      tree.train(sample_data_vec);
+      }
   }
+int predict (const vector <double>& feature){
+    vector<int> vote(num_trees_, 0);
+    for (auto& tree:trees_){
+        int prediction = tree.predict(feature);
+        vote[prediction] ++;
+    }
+return distance (votes.begin(), max_element(votes.begin(), votes.end()));
 }
 
-int RandomForest::predict(const vector<double>& feature){
-    vector<int> votes (num_trees_, 0);
-    for (auto& tree:trees_){
-      int prediction = tree.predict(feature);
-      vote[prediction]++;
-    }
-return distance(vote.begin(), max_element(votes.begin(), vote.end()));
-}
+private:
+  int num_trees_;
+  vector <DTree> trees_;
+};
 
