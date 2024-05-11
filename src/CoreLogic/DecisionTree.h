48a4cf83d18c30829faf12288e1dc1e4524da4fe
Victor Li <113569505+qasdasd1020@users.noreply.github.com>
2024-05-10 23:13:37 -0400

    Update DecisionTree.h

--- src/CoreLogic/DecisionTree.h
+++ src/CoreLogic/DecisionTree.h
@@ -7,7 +7,7 @@
 
 class DecisionTree {
 public:
-  DecisionTree() : root (new Node()){}
+  DecisionTree() : root (new Node()){}                                    //Constructor initializes the tree with a root node
   void train(const vector<vector<double>>& data_vec){
     cout << "Training Decision Tree..." <<endl;
     for (size_t row = 0; row < data_vec.size(); row++){
@@ -18,11 +18,11 @@ public:
   }
 int predict(const vector<double>& feature){
   cout<<"Predicting with Decision Trees..." <<endl;
-  return 0;
+  return 0;                                                        //placeholder return
 }
 private:
-  unique_ptr<Node> root;
-  void build_tree(Node*& node, const vector<vector<double>>& data_vec, size_t start, size_t end);
+  unique_ptr<Node> root;                                          //Unique pointer to the root node of decision tree
+  void build_tree(Node*& node, const vector<vector<double>>& data_vec, size_t start, size_t end);                      //Recursive function to build the tree
 };
 
 #endif
