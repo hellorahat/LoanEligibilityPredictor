d9ab9000da3d07afc28a9d4f469b4c22a73c0e9d
hellorahat <rsazad813@gmail.com>
2024-05-12 02:17:23 -0400

    Update main.cpp
    added descriptive comments

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ int main(int argc, char* argv[]) {
     categorical_indexes.push_back(0); 
     categorical_indexes.push_back(1);
 
+    // call data processor
     std::tuple<std::vector<std::string>, std::vector<std::vector<double>>, std::vector<double>> tuple_contents = data_handler.process_data(training_data, categorical_indexes);
-    std::vector<std::string> feature_name_vec = std::get<0>(tuple_contents);
-    std::vector<std::vector<double>> double_vec = std::get<1>(tuple_contents);
-    std::vector<double> impute_vec = std::get<2>(tuple_contents);
+    // place the information obtained from the function into variables
+    std::vector<std::string> feature_name_vec = std::get<0>(tuple_contents); // the header row: this contains the column names
+    std::vector<std::vector<double>> double_vec = std::get<1>(tuple_contents); // the raw data content
+    std::vector<double> impute_vec = std::get<2>(tuple_contents); // a vector that will be used to replace missing values with, uses mean for numerical, and mode for categorical.
 
     data_handler.vector_to_csv(feature_name_vec, double_vec, "one-hot-encoded-vector");
-
-
 
     // split the data into training and testing vectors
 
