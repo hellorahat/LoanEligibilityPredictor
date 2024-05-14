9b650a6bd5ad53a3f5bcc8b22a04110fca55aa5a
Victor Li <113569505+qasdasd1020@users.noreply.github.com>
2024-05-14 01:09:19 -0400

    Update Node.h

--- src/CoreLogic/Node.h
+++ src/CoreLogic/Node.h
@@ -1,34 +1,47 @@
-//Node.h
-/**
-  *@file Node.h
-  *@brief Header file for the Node class used in decision trees.
-  *Contains both declaration and implementation.
-  */
-
 #ifndef NODE_H
 #define NODE_H
 
 #include <memory>
 #include <vector>
 
-//**
-  *@class Node
-  *@brief Represent a node in a decision tree.
-  *
-  *This class encapsulates the properites of a node in a decision tree, including pointers to child nodes, feature index for splitting, threshold value, and
-  *whether it is a leaf node.
-  */
+/**
+ * @file Node.h
+ * @brief Header file for the Node class used in decision trees.
+ * Contains both declaration and implementation.
+*/
+
+using namespace std;
+
+/**
+ * @class Node
+ * @brief Node class for decision tree nodes.
+ * This class represents a node in the decision tree.
+*/
 
 class Node{
 public:
-  Node(): left(nullptr), right(nullptr), feature_index(-1), threshold(0.0), is_leaf(false), label(-1), gini_index(0){}
+    Node() : left(nullptr), right(nullptr), feature_index(-1), threshold(0.0), is_leaf(false), label(-1), gini_index(0.0){}
 
-  unique_ptr<Node> left;                               //pointer to the left child node
-  unique_ptr<Node> right;                              //pointer to the right child node
-  int feature_index;                                   //Index of feature based on which the node splits
-  double threshold                                     //Threshold value for splitting at this node
-  bool is_leaf;                                        //Flag to check if the node is a leaf node
-  int label;                                          //Class label assigned to a leaf node
-  int gini_index;                                      // Gini index for the node, used in splitting criteria
+    /// @brief Pointer to the left child node
+    unique_ptr<Node> left;
+
+    /// @brief Pointer to the right child node
+    unique_ptr<Node> right;
+
+    /// @brief Index of the feature based on which the node splits
+    int feature_index;
+
+    /// @brief Threshold value for splitting at this node
+    double threshold;
+
+    /// @brief Flag to check if the node is a leaf node
+    bool is_leaf;
+
+    /// @brief class label assigned to a leaf node
+    int label;
+
+    /// @brief Gini index for the node, used in splitting criteria
+    double gini_index;
+};
 
 #endif      //NODE_H
