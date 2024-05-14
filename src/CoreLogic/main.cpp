69e5fedeca20ba843d55d177589312c2b3996d62
Victor Li <113569505+qasdasd1020@users.noreply.github.com>
2024-05-14 02:55:15 -0400

    Create main.cpp

---
+++ src/CoreLogic/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include "RandomForest.h"
+#include "..\DataProcessing\DataHandler.h"
+
+int main(){
+    DataHandler data_handler;
+    std::vector<int> categorical_indexes = {0, 1};        //adjust based on your dataset
+    std::string filename = "~GitHub\LoanRepaymentPredictor\data\loan_data.csv";                          //Update this path
+    std::ifstream file(filename);
+    if (!file) {
+        std:: cerr << "Error opening file!" << std:: endl;
+        return 1;
+    }
+
+    //Process the data
+    DataFrame *df = data_handler.process_data (filename, categorical_indexes);
+    std::vector<std::vector<double>> data = df -> get_data_vec();
+
+    //initialize RandomForest
+    int num_trees = 10;         //Number of trees in the forest.
+    RandomForest forest(num_trees);
+
+    //Train the RandomForest
+    forest.train(data);
+
+    //optionally, evaluate the model if you have a separate test set
+    double accuracy = forest.evaluate(data);        //here, using the same data for simplicity
+    std:: cout<< "Model Accuracy: " << accuracy * 100 << "%" << std:: endl;
+
+    delete df; //Clean up DataFrame object
+    file.close();
+    return 0;
+}
